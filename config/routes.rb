c54be63f96106fc94075c435840f71d2e5c4c986
Matt Schallert <mschallert@yammer-inc.com>
2013-08-06 11:38:53 -0700

    MAKE DEVISE ROUTES NICE WOO

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Pinstopaper::Application.routes.draw do
   root :to => 'static_pages#home'
 
-  devise_for :users
+  devise_for :users, :skip => [:sessions]
+  as :user do
+    get 'login' => 'devise/sessions#new', :as => :new_user_session
+    post 'login' => 'devise/sessions#create', :as => :user_session
+    get 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 end
