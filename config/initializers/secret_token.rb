189379f4a57928b7af641bf242ad1e9d48c7f852
Matt Schallert <mschallert@yammer-inc.com>
2013-08-06 11:03:23 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinstopaper::Application.config.secret_key_base = '97e178c3e643abd3c9672da7f7ae893377cb017cf7c3d84be3fdc5359ff7c3c5f67ed4da68b3f79eb34a888dae8e1f4d59fdab88ce0d5e4c0a21498a26675c32'
