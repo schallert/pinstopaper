d59e5b7e85945c7b5fdae720601461cb919298de
Matt Schallert <mschallert@yammer-inc.com>
2013-08-06 13:23:10 -0700

    Add pinboard import methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,27 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :pinboard_posts
+
+  def posts_from_pinboard
+    @pinboard ||= Pinboard::Client.new(:token => self.pinboard_token)
+    @pinboard_posts ||= @pinboard.posts
+    @pinboard_posts
+  end
+
+  def import_pinboard_posts
+    posts_from_pinboard.each do |post_res|
+      post = PinboardPost.create(
+        :href        => post_res.href,
+        :description => post_res.description,
+        :extended    => post_res.extended,
+        :time        => post_res.time,
+        :replace     => post_res.replace == "yes",
+        :shared      => post_res.shared == "yes",
+        :toread      => post_res.toread == "yes",
+        :user        => self
+      )
+    end
+  end
 end
