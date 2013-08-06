90ac1aca80a93d93ea2323e5b22ad0a99765de3d
Matt Schallert <mschallert@yammer-inc.com>
2013-08-06 15:32:51 -0700

    Always show something for a title even if blank

--- app/models/pinboard_post.rb
+++ app/models/pinboard_post.rb
@@ -4,4 +4,12 @@ class PinboardPost < ActiveRecord::Base
   belongs_to :user
 
   scope :unread, -> { where(:toread => true) }
+
+  def formatted_description
+    if self.description.blank?
+      "(Empty)"
+    else
+      self.description
+    end
+  end
 end
