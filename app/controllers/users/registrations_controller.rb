e2373d1e8c086dd9f46a312e6e3ec01898bc0ff0
Matt Schallert <mschallert@yammer-inc.com>
2013-08-06 17:32:49 -0700

    Implement editing of pinboard_token

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,4 +3,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def sign_up_params
      params.require(:user).permit(:email, :password, :password_confirmation, :pinboard_token)
   end
+
+  def account_update_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :pinboard_token)
+  end
 end
