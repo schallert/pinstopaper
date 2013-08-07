4fa1ad01e9a8bf7f303eee85ca131fead9cf2992
Matt Schallert <mschallert@yammer-inc.com>
2013-08-07 11:43:57 -0700

    Validate uniqueness on post hrefs per user

--- app/models/pinboard_post.rb
+++ app/models/pinboard_post.rb
@@ -5,6 +5,9 @@ class PinboardPost < ActiveRecord::Base
 
   scope :unread, -> { where(:toread => true) }
 
+  # Last defense in preventing importing a post multiple times
+  validates :href, :uniqueness => { :scope => :user_id }
+
   def formatted_description
     if self.description.blank?
       "(Empty)"
